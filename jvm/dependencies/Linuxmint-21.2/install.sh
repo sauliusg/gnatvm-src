4dddced1be64ef326fed9916a784950ffd1fc618
saulius <saulius>
2024-08-19 10:52:47 +0300

    Adding the dependency installation script for Debian-like systems (Linuxmint-21.2).

---
+++ jvm/dependencies/Linuxmint-21.2/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install dependencies available trough the standard package
+# management system on Debian and Debian-like systems.
+
+sudo apt-get install -y \
+     libiberty-dev \
+     #
