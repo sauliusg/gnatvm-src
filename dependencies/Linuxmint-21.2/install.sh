a6307b65cf2eb0e580b82c1e789b3aac01c15b93
saulius <saulius>
2024-08-21 18:10:01 +0300

    Removing the dependency installation script since libiberty.a is no longer used.

--- dependencies/Linuxmint-21.2/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Install dependencies available trough the standard package
-# management system on Debian and Debian-like systems.
-
-sudo apt-get install -y \
-     libiberty-dev \
-     #
